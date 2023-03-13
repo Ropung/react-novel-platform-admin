9936d58eb6815c18e298c73aab5eefd2a6273022
Gihun <563710@naver.com>
2023-03-13 19:17:55 +0900

    feat(users): style adjust

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import GNB from "./components/common/GNB";
 
 function App() {
   const location = useLocation();
-  const { HOME } = Path;
 
   const [hasSideNav, setHasSideNav] = useState<boolean>(false);
 
@@ -25,20 +24,9 @@ function App() {
     !isLogin && setRoutesComponent(<UnauthenticatedRoutes />);
   }, [isLogin]);
 
-  useLayoutEffect(() => {
-    const pathname =
-      location.pathname.endsWith("/") && location.pathname.length > 1
-        ? location.pathname.slice(0, -1)
-        : location.pathname;
-
-    const hasSideNav = [HOME].includes(pathname);
-
-    setHasSideNav(hasSideNav);
-  }, []);
-
   return (
     <main className="w-full h-full min-h-screen">
-      <div className="w-full max-w-screen h-full flex flex-row">
+      <div className="w-full min-w-full h-full flex flex-row">
         {/* 사이드 */}
         <aside
           className={`w-72 min-h-screen bg-primary duration-300 whitespace-nowrap ${
@@ -50,16 +38,20 @@ function App() {
 
         {/* 메인 */}
         <section
-          className={`flex flex-col h-full min-h-screen ${
-            hasSideNav ? "!w-[calc(100% - 18rem)]" : "!w-full"
+          className={`w-[calc(100%-18rem)] flex flex-1 flex-col h-full min-h-screen ${
+            hasSideNav ? "" : ""
           }`}
         >
           {/* 헤더 */}
-          <header className={"w-full h-14 bg-white border-b-2 shadow-xl"}>
+          <header
+            className={`w-full h-14 bg-white border-b-2 shadow-xl ${
+              hasSideNav ? "w-[calc(100% - 18rem)]" : "!w-full"
+            }`}
+          >
             <GNB hasSideNav={hasSideNav} setHasSideNav={setHasSideNav} />
           </header>
           {/* 컨테이너 */}
-          <section className="w-full flex flex-1 justify-center items-center p-4 bg-gray-100">
+          <section className="w-full flex flex-1 justify-center items-start p-4 bg-gray-100">
             {RoutesComponent}
           </section>
         </section>
--- src/components/routes/UnauthenticatedRoutes.tsx
+++ src/components/routes/UnauthenticatedRoutes.tsx
@@ -1,13 +1,15 @@
 import HomePage from "@/pages/home/HomePage";
 import Path from "@/utils/path/routes";
 import { Route, Routes } from "react-router-dom";
+import UserManagement from "../users/UserManagement";
 
 const UnauthenticatedRoutes = () => {
-  const { HOME } = Path;
+  const { HOME, USERS } = Path;
 
   return (
     <Routes>
       <Route path={HOME} element={<HomePage />} />
+      <Route path={USERS} element={<UserManagement />} />
     </Routes>
   );
 };
