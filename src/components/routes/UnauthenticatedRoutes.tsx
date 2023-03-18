b0715fed5fb4ebd1902a372b5cee8c802b1147f3
Gihun <563710@naver.com>
2023-03-19 00:47:50 +0900

    feat(routes): add components

--- src/components/routes/UnauthenticatedRoutes.tsx
+++ src/components/routes/UnauthenticatedRoutes.tsx
@@ -1,15 +1,35 @@
 import HomePage from "@/pages/home/HomePage";
 import Path from "@/utils/path/routes";
 import { Route, Routes } from "react-router-dom";
+import UserGradeManagement from "../users/UserGradeManagement";
 import UserManagement from "../users/UserManagement";
+import UserAuthorWriterManagement from "../users/UserAuthorWriterManagement";
+import UserAuthorWriterRequest from "../users/UserAuthorWriterRequest";
+import NobelManagement from "../novel/NobelManagement";
 
 const UnauthenticatedRoutes = () => {
-  const { HOME, USERS } = Path;
+  const {
+    HOME,
+    USERS,
+    GRADE,
+    AUTH_WRITER_REQ,
+    AUTH_WRITER_MANAGEMENT,
+    AUTH_NOVEL_MANAGEMENT,
+  } = Path;
 
   return (
     <Routes>
       <Route path={HOME} element={<HomePage />} />
       <Route path={USERS} element={<UserManagement />} />
+      <Route path={GRADE} element={<UserGradeManagement />} />
+      {/* 작가 신청,관리 */}
+      <Route path={AUTH_WRITER_REQ} element={<UserAuthorWriterRequest />} />
+
+      <Route
+        path={AUTH_WRITER_MANAGEMENT}
+        element={<UserAuthorWriterManagement />}
+      />
+      <Route path={AUTH_NOVEL_MANAGEMENT} element={<NobelManagement />} />
     </Routes>
   );
 };
