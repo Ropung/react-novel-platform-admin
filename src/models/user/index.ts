86d70b043c85e81dc32ab8657f50763900221da1
Gihun <563710@naver.com>
2023-03-19 00:49:57 +0900

    feat(models): UserInfo 모델정의

---
+++ src/models/user/index.ts
@@ -0,0 +1,35 @@
+export default interface UserInfoProps {
+  id: number;
+  userName: string;
+  email: string;
+  states: StatePops;
+  job: JobProps;
+  grade: gradeProps;
+  signIn: string;
+  lastIn: string;
+  novel: novelProps[];
+}
+
+export interface gradeProps {
+  gradeName?: GradeNameProps;
+  gradeLevel?: number;
+  discountRate?: number;
+}
+
+export interface novelProps {
+  userName: string;
+  novelId: number;
+  novelTitle: string;
+  category: categoryProps[];
+}
+
+export type GradeNameProps =
+  | "BRONZE"
+  | "SILVER"
+  | "GOLD"
+  | "PLATINUM"
+  | "DIAMOND";
+export type StatePops = "Approved" | "Declined";
+export type JobProps = "USER" | "MAKER";
+
+export type categoryProps = "Action" | "Romance" | "Thriller";
