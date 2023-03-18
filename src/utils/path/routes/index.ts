69cc3fa46cafb64a2fa1217f39b7e2593ddbf407
Gihun <563710@naver.com>
2023-03-19 00:48:32 +0900

    feat(path): add path(작가신청,관리,등급)

--- src/utils/path/routes/index.ts
+++ src/utils/path/routes/index.ts
@@ -1,6 +1,10 @@
 const Path = {
   HOME: "/",
   USERS: "/users",
+  GRADE: "/garde",
+  AUTH_WRITER_REQ: "/write-req",
+  AUTH_WRITER_MANAGEMENT: "/write-management",
+  AUTH_NOVEL_MANAGEMENT: "/novel-management",
 };
 
 Object.freeze(Path);
